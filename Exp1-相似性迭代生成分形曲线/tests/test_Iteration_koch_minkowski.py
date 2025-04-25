41ab06d2509745453dbef27afea4cf1c01ef62b6
kuku0320 <3287436946@qq.com>
2025-04-25 23:58:59 +0800

    Update test_Iteration_koch_minkowski.py

--- Exp1-相似性迭代生成分形曲线/tests/test_Iteration_koch_minkowski.py
+++ Exp1-相似性迭代生成分形曲线/tests/test_Iteration_koch_minkowski.py
@@ -1,91 +1,87 @@
+import numpy as np
 import matplotlib.pyplot as plt
-import math
 
-def apply_rules(axiom, rules, iterations):
-    """
-    L-System string generator
-    :param axiom: Initial string
-    :param rules: Dictionary, symbol rewriting rules
-    :param iterations: Number of iterations
-    :return: Generated string after iterations
-    """
-    current = axiom
-    for _ in range(iterations):
-        next_seq = []
-        for c in current:
-            next_seq.append(rules.get(c, c))
-        current = ''.join(next_seq)
-    return current
+def koch_generator(u, level):
+    u = np.array([0, 1j]) # 初始竖直线段
+    
+    if level <= 0:
+        return u
+        
+    theta = np.pi/3 # 旋转角度
+    for _ in range(level):
+        new_u = []
+        for i in range(len(u)-1):
+            start = u[i]
+            end = u[i+1]
+            
+            # 生成科赫曲线的四个新线段
+            p1 = start
+            p2 = start + (end - start)/3
+            p3 = p2 + (end - start)/3 * np.exp(1j*theta)
+            p4 = start + 2*(end - start)/3
+            p5 = end
+            
+            new_u.extend([p1, p2, p3, p4, p5])
+        
+        u = np.array(new_u)
+    
+    return u
 
-def draw_l_system(commands, angle_deg, step, initial_pos=(0, 0), initial_angle=90, tree_mode=False, savefile=None):
-    """
-    L-System plotter
-    :param commands: Command string
-    :param angle_deg: Angle to turn each time
-    :param step: Step length
-    :param initial_pos: Initial position
-    :param initial_angle: Initial direction (degrees)
-    :param tree_mode: Whether to use fractal tree mode (affects behavior of [ and ])
-    :param savefile: If specified, save the plot to this file
-    """
-    x, y = initial_pos
-    current_angle = initial_angle
-    stack = []
-    fig, ax = plt.subplots()
-    for cmd in commands:
-        if cmd in ('F', '0', '1'):
-            nx = x + step * math.cos(math.radians(current_angle))
-            ny = y + step * math.sin(math.radians(current_angle))
-            ax.plot([x, nx], [y, ny], color='green' if tree_mode else 'blue', linewidth=1.2 if tree_mode else 1)
-            x, y = nx, ny
-        elif cmd == 'f':
-            x += step * math.cos(math.radians(current_angle))
-            y += step * math.sin(math.radians(current_angle))
-        elif cmd == '+':
-            current_angle += angle_deg
-        elif cmd == '-':
-            current_angle -= angle_deg
-        elif cmd == '[':
-            stack.append((x, y, current_angle))
-            if tree_mode:
-                current_angle += angle_deg
-        elif cmd == ']':
-            x, y, current_angle = stack.pop()
-            if tree_mode:
-                current_angle -= angle_deg
-    ax.set_aspect('equal')
-    ax.axis('off')
-    if savefile:
-        plt.savefig(savefile, bbox_inches='tight', pad_inches=0.1, dpi=150)
-        plt.close()
-    else:
-        plt.show()
+def minkowski_generator(u, level):
+    u = np.array([0, 1]) # 初始水平线段
+    
+    theta = np.pi/2 # 旋转角度
+    for _ in range(level):
+        new_u = []
+        for i in range(len(u)-1):
+            start = u[i]
+            end = u[i+1]
+            
+            # 生成Minkowski曲线的八个新线段
+            p1 = start
+            p2 = start + (end - start)/4
+            p3 = p2 + (end - start)/4 * np.exp(1j*theta)
+            p4 = p2 + (end - start)/4 * (1 + 1j)
+            p5 = start + (end - start)/2 + (end - start)/4 * 1j
+            p6 = start + (end - start)/2
+            p7 = start + (end - start)/2 - (end - start)/4 * 1j
+            p8 = start + 3*(end - start)/4 - (end - start)/4 * 1j
+            p9 = start + 3*(end - start)/4
+            p10 = end
+            
+            new_u.extend([p1, p2, p3, p4, p5, p6, p7, p8, p9, p10])
+        
+        u = np.array(new_u)
+    
+    return u
+
 
 if __name__ == "__main__":
-    # Koch curve parameters
-    koch_axiom = "F"
-    koch_rules = {'F': 'F+F--F+F'}
-    koch_angle = 60
-    koch_iter = 4
-    koch_step = 5
-    koch_cmds = apply_rules(koch_axiom, koch_rules, koch_iter)
-    plt.figure(figsize=(10, 3))
-    draw_l_system(koch_cmds, koch_angle, koch_step, initial_pos=(0, 0), initial_angle=0)
-    plt.title("L-System Koch Curve")
-    plt.axis('equal')
-    plt.axis('off')
+    # 初始线段
+    init_u = np.array([0, 1])
+    
+    # 创建2x2子图布局
+    fig, axs = plt.subplots(2, 2, figsize=(10, 10))
+    
+    # 生成不同层级的科赫曲线
+    for i in range(4):
+        koch_points = koch_generator(init_u, i+1)
+        axs[i//2, i%2].plot(koch_points.real, koch_points.imag, 'k-', lw=1)
+        axs[i//2, i%2].set_title(f"Koch Curve Level {i+1}")
+        axs[i//2, i%2].axis('equal')
+        axs[i//2, i%2].axis('off')
+    
+    plt.tight_layout()
     plt.show()
 
-    # Fractal tree parameters
-    tree_axiom = "0"
-    tree_rules = {'1': '11', '0': '1[0]0'}
-    tree_angle = 45
-    tree_iter = 7
-    tree_step = 7
-    tree_cmds = apply_rules(tree_axiom, tree_rules, tree_iter)
-    plt.figure(figsize=(7, 7))
-    draw_l_system(tree_cmds, tree_angle, tree_step, initial_pos=(0, 0), initial_angle=90, tree_mode=True)
-    plt.title("L-System Fractal Tree")
-    plt.axis('equal')
-    plt.axis('off')
+    # 生成不同层级的Minkowski香肠
+    fig, axs = plt.subplots(2, 2, figsize=(10, 10))
+    for i in range(4):
+        minkowski_points = minkowski_generator(init_u, i+1)
+        axs[i//2, i%2].plot(minkowski_points.real, minkowski_points.imag, 'k-', lw=1)
+        axs[i//2, i%2].set_title(f"Minkowski Sausage Level {i+1}")
+        axs[i//2, i%2].axis('equal')
+        axs[i//2, i%2].axis('off')
+    
+    plt.tight_layout()
     plt.show()
